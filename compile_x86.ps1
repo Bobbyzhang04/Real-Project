87830b8145e241957c8f14e8c39f4a41fdbbd5e1
Johnathan Louie <johnathanlouie88@gmail.com>
2021-11-10 19:05:50 -0800

    Add delete files to rebuild scripts

--- compile_x86.ps1
+++ compile_x86.ps1
@@ -1,1 +1,4 @@
+Remove-Item spec/x86 -Recurse
+Remove-Item dist/x86 -Recurse
+Remove-Item build/x86 -Recurse
 pyinstaller --specpath spec/x86 --distpath dist/x86 --workpath build/x86 --onefile main.py
--- msix.ps1
+++ msix.ps1
@@ -1,3 +1,5 @@
-makeappx pack /o /m appxmanifest64.xml /f mapping_x64.txt /p msix/xcleaner_x64.msix
-makeappx pack /o /m appxmanifest86.xml /f mapping_x86.txt /p msix/xcleaner_x86.msix
-makeappx bundle /o /d msix /p xcleaner.msixbundle
+Remove-Item msixpackage -Recurse
+Remove-Item msixbundle -Recurse
+makeappx pack /o /m appxmanifest64.xml /f mapping_x64.txt /p msixpackage/xcleaner_x64.msix
+makeappx pack /o /m appxmanifest86.xml /f mapping_x86.txt /p msixpackage/xcleaner_x86.msix
+makeappx bundle /o /d msixpackage /p msixbundle/xcleaner.msixbundle
